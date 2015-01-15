13bdcc91801d9da65a6cfb8027563daec0a83e5a
Moez Bouhlel <bmoez.j@gmail.com>
2015-01-15 21:39:10 +0000

    add documentation to core.c

--- src/core.c
+++ src/core.c
@@ -1,20 +1,39 @@
 #include "core.h"
 
+/*
+ * init the Core, It should init the core struct on the future
+ * return 0 on success
+ */
 int initCore()
 {
 	return 0;
 }
 
+/*
+ * add Money to the main budget
+ * parm count: added money count
+ * return 0 on success
+ */
 int addMoney(float count)
 {
 	return 0;
 }
 
+/*
+ * sub Money from the main budget
+ * parm count: subed money count
+ * return 0 on success
+ */
 int subMoney(float count)
 {
 	return 0;
 }
 
+/*
+ * show the statics, it should accept a pointer to an struct to set it values on
+ * the future
+ * return 0 on success
+ */
 int showStatics()
 {
 	return 0;
